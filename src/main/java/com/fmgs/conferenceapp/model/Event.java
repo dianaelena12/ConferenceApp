952200ec4a96f5a9cd67d1cbef7872eec85b56ad
Diana Mucilenita <mucilenita.diana@gmail.com>
2019-05-25 19:43:23 +0300

    Created Event and PresentationSchedule class

---
+++ src/main/java/com/fmgs/conferenceapp/model/Event.java
@@ -0,0 +1,40 @@
+package com.fmgs.conferenceapp.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Event {
+    @JsonProperty("schedule")
+    private PresentationSchedule presentationSchedule;
+    private String eventName;
+    private String speaker;
+
+    public Event(PresentationSchedule presentationSchedule, String eventName, String speaker) {
+        this.presentationSchedule = presentationSchedule;
+        this.eventName = eventName;
+        this.speaker = speaker;
+    }
+
+    public PresentationSchedule getPresentationSchedule() {
+        return presentationSchedule;
+    }
+
+    public void setPresentationSchedule(PresentationSchedule presentationSchedule) {
+        this.presentationSchedule = presentationSchedule;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getSpeaker() {
+        return speaker;
+    }
+
+    public void setSpeaker(String speaker) {
+        this.speaker = speaker;
+    }
+}
---
+++ src/main/java/com/fmgs/conferenceapp/model/PresentationSchedule.java
@@ -0,0 +1,27 @@
+package com.fmgs.conferenceapp.model;
+
+public class PresentationSchedule {
+    private String startHour;
+    private String endHour;
+
+    public PresentationSchedule(String startHour, String endHour) {
+        this.startHour = startHour;
+        this.endHour = endHour;
+    }
+
+    public String getStartHour() {
+        return startHour;
+    }
+
+    public void setStartHour(String startHour) {
+        this.startHour = startHour;
+    }
+
+    public String getEndHour() {
+        return endHour;
+    }
+
+    public void setEndHour(String endHour) {
+        this.endHour = endHour;
+    }
+}
