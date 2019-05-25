e03d27d1e50ec53e8499a87cd960850f761cbb57
Muradasil Birhan <b98lion@gmail.com>
2019-05-25 20:09:47 +0300

    Date/Confernce classess

--- src/main/java/com/fmgs/conferenceapp/model/Conference.java
+++ src/main/java/com/fmgs/conferenceapp/model/Conference.java
@@ -1,4 +1,44 @@
 package com.fmgs.conferenceapp.model;
 
+import java.util.Map;
+
 public class Conference {
+
+    private Date date;
+    private Map<String,Event> itinerary;
+    private String eventName;
+
+    public Conference(Date date, Map<String, Event> itinerary, String eventName) {
+        this.date = date;
+        this.itinerary = itinerary;
+        this.eventName = eventName;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void reserveRoomForEvent(String room, Event event){
+        itinerary.put(room,event);
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public Map<String, Event> getItinerary() {
+        return itinerary;
+    }
+
+    public void setItinerary(Map<String, Event> itinerary) {
+        this.itinerary = itinerary;
+    }
 }
